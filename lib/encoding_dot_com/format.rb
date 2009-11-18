6ea776bf7bd708794868a1f1c1744b9c7b71a51b
Roland Swingler <roland.swingler@gmail.com>
2009-11-18 14:46:10 +0000

    Basic size sttribute

--- lib/encoding_dot_com/format.rb
+++ lib/encoding_dot_com/format.rb
@@ -9,13 +9,28 @@ module EncodingDotCom
     
     def initialize(attributes={})
       @output = attributes["output"]
+      @width = attributes["width"] || 0
+      @height = attributes["height"] || 0
       raise IllegalFormatAttribute.new unless ALLOWED_OUTPUT_FORMATS.include?(@output)
     end
-    
+
+    def size
+      "#{@width}x#{@height}" if size_specified?
+    end
+
     def build_xml(builder)
       builder.format {
         builder.output @output
+        if size_specified?
+          builder.size self.size
+        end
       }
+    end
+
+    private
+
+    def size_specified?
+      @height != 0 || @width != 0
     end
   end
 end
--- spec/format_spec.rb
+++ spec/format_spec.rb
@@ -2,21 +2,41 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "Encoding.com Format" do
 
-  it "should have an output attribute" do
-    EncodingDotCom::Format.new("output" => "flv").output.should == "flv"
+  describe "output" do
+    it "should have a output attribute" do
+      EncodingDotCom::Format.new("output" => "flv").output.should == "flv"
+    end
+
+    it "should be required" do
+      lambda { EncodingDotCom::Format.new }.should raise_error(EncodingDotCom::IllegalFormatAttribute)
+    end
+
+    it "should not allow an illegal format" do
+      lambda { EncodingDotCom::Format.new("output" => "foo") }.should raise_error(EncodingDotCom::IllegalFormatAttribute)
+    end
+
+    it "should allow flv, fl9, wmv, 3gp, mp4, m4v, ipod, iphone, appletv, psp, zune, mp3, wma and thumbnail output formats" do
+      %w{flv fl9 wmv 3gp mp4 m4v ipod iphone appletv psp zune mp3 wma thumbnail}.each do |format|
+        lambda { EncodingDotCom::Format.new("output" => format) }.should_not raise_error
+      end
+    end
   end
 
-  it "should require an output format" do
-    lambda { EncodingDotCom::Format.new }.should raise_error(EncodingDotCom::IllegalFormatAttribute)
-  end
+  describe "specifying the size of the output video" do
+    it "should be specifiable with a width only" do
+      EncodingDotCom::Format.new("output" => "flv", "width" => 640).size.should == "640x0"
+    end
 
-  it "should not allow an illegal output format" do
-    lambda { EncodingDotCom::Format.new("output" => "foo") }.should raise_error(EncodingDotCom::IllegalFormatAttribute)
-  end
+    it "should be specifiable with a height only" do
+      EncodingDotCom::Format.new("output" => "flv", "height" => 480).size.should == "0x480"
+    end
 
-  it "should allow flv, fl9, wmv, 3gp, mp4, m4v, ipod, iphone, appletv, psp, zune, mp3, wma and thumbnail output formats" do
-    %w{flv fl9 wmv 3gp mp4 m4v ipod iphone appletv psp zune mp3 wma thumbnail}.each do |format|
-      lambda { EncodingDotCom::Format.new("output" => format) }.should_not raise_error
+    it "should be specifiable both by width and height" do
+      EncodingDotCom::Format.new("output" => "flv", "width" => 640, "height" => 480).size.should == "640x480"
+    end
+    
+    it "should be nil if neither width nor height are specified" do
+      EncodingDotCom::Format.new("output" => "flv").size.should be_nil
     end
   end
 
@@ -33,6 +53,20 @@ describe "Encoding.com Format" do
       Nokogiri::XML::Builder.new do |b|
         format.build_xml(b)
       end.to_xml.should have_xpath("/format/output[text()='flv']")
+    end
+
+    it "should not create a size node if size is not specified" do
+      format = EncodingDotCom::Format.new("output" => "flv")
+      Nokogiri::XML::Builder.new do |b|
+        format.build_xml(b)
+      end.to_xml.should_not have_xpath("/format/size")
+    end
+
+    it "should create a size node" do
+      format = EncodingDotCom::Format.new("output" => "flv", "height" => 480)
+      Nokogiri::XML::Builder.new do |b|
+        format.build_xml(b)
+      end.to_xml.should have_xpath("/format/size[text()='0x480']")
     end
   end
 end
