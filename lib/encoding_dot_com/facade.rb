6e0c81014d2ffcd540902a437edb111eed620031
Roland Swingler <roland.swingler@gmail.com>
2009-11-18 10:59:01 +0000

    Adding some documation

--- lib/encoding_dot_com/facade.rb
+++ lib/encoding_dot_com/facade.rb
@@ -1,15 +1,28 @@
 require 'nokogiri'
 
 module EncodingDotCom
+  # Error raised if there is an http-level problem accessing the
+  # encoding.com API.
   class AvailabilityError < StandardError
   end
 
+  # Error raiseed if there is an problem with the message sent to
+  # encoding.com API.
   class MessageError < StandardError
   end
-  
+
+  # A remote facade to the encoding.com API.
+  #
+  # The facade is stateless and can be reused for multiple requests.
   class Facade
+    # Where encoding.com expects messages to be posted to.
     ENDPOINT = "http://manage.encoding.com/"
-    
+
+    # Creates a new facacde given an encoding.com user id & key, and
+    # an HTTP library implementation.
+    #
+    # +http+ should respond to post, and return an object responding to
+    # +#code+ and +#to_s+
     def initialize(user_id, user_key, http)
       @user_id, @user_key, @http = user_id, user_key, http
     end
