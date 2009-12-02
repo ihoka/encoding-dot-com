cf076cc9f5f8d20931fa938063b111011aef8fb4
Roland Swingler <roland.swingler@gmail.com>
2009-12-02 16:36:22 +0000

    Changing methods on MediaListItem to be snake_cased

--- lib/encoding_dot_com/media_list_item.rb
+++ lib/encoding_dot_com/media_list_item.rb
@@ -1,15 +1,22 @@
 module EncodingDotCom
+
+  # Represents a video or image in the encoding.com queue
   class MediaListItem
-    attr_reader :mediafile, :mediaid, :mediastatus, :createdate, :startdate, :finishdate
-  
+    attr_reader :media_file, :media_id, :media_status, :create_date, :start_date, :finish_date
+
+    # Creates a MediaListItem, given a <media> Nokogiri::XML::Node
+    #
+    # See the encoding.com documentation for GetMediaList for more details
     def initialize(node)
-      @mediafile = (node / "mediafile").text
-      @mediaid = (node / "mediaid").text.to_i
-      @mediastatus = (node / "mediastatus").text
-      @createdate = parse_time_node(node / "createdate")
-      @startdate = parse_time_node(node / "startdate")
-      @finishdate = parse_time_node(node / "finishdate")
+      @media_file = (node / "mediafile").text
+      @media_id = (node / "mediaid").text.to_i
+      @media_status = (node / "mediastatus").text
+      @create_date = parse_time_node(node / "createdate")
+      @start_date = parse_time_node(node / "startdate")
+      @finish_date = parse_time_node(node / "finishdate")
     end
+
+    private
 
     def parse_time_node(node)
       Time.local *ParseDate.parsedate(node.text)
--- spec/facade_spec.rb
+++ spec/facade_spec.rb
@@ -134,27 +134,27 @@ describe "Encoding.com Facade" do
       end
       
       it "should have a hash of returned attributes with a mediafile key" do
-        @facade.list.first.mediafile.should == "foo.wmv"
+        @facade.list.first.media_file.should == "foo.wmv"
       end
       
       it "should have a hash of returned attributes with a mediaid key" do
-        @facade.list.first.mediaid.should == 1234
+        @facade.list.first.media_id.should == 1234
       end
       
       it "should have a hash of returned attributes with a mediastatus key" do
-        @facade.list.first.mediastatus.should == "Closed"
+        @facade.list.first.media_status.should == "Closed"
       end
       
       it "should have a hash of returned attributes with a createdate key" do
-        @facade.list.first.createdate.should == Time.local(2009, 1, 1, 12, 0, 1)
+        @facade.list.first.create_date.should == Time.local(2009, 1, 1, 12, 0, 1)
       end
 
       it "should have a hash of returned attributes with a startdate key" do
-        @facade.list.first.startdate.should == Time.local(2009, 1, 1, 12, 0, 2)
+        @facade.list.first.start_date.should == Time.local(2009, 1, 1, 12, 0, 2)
       end
 
       it "should have a hash of returned attributes with a finishdate key" do
-        @facade.list.first.finishdate.should == Time.local(2009, 1, 1, 12, 0, 3)
+        @facade.list.first.finish_date.should == Time.local(2009, 1, 1, 12, 0, 3)
       end
     end
   end
